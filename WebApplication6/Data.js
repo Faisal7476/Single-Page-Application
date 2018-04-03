bb3da38498c48f5b7cd7ba1c529ecff907f0bc00
Faisal7476 <34696852+Faisal7476@users.noreply.github.com>
2018-04-03 00:45:11 -0400

    first commit

---
+++ WebApplication6/Data.js
@@ -0,0 +1,27 @@
+﻿//var myapp = angular.module('myapp', []);
+//myapp.controller('ctrl', function ($scope)
+//{
+//    $scope.Login = function ()
+//    {
+//        if ($scope.txtun == 'faisal.khan' &&
+//            $scope.txtpw == '12345')
+//{
+//    alert('login success!');
+//}
+//else
+//{
+//    alert('loginfailed!');
+//}
+//    }
+//});
+
+
+var myapp = angular.module('myapp', []);
+myapp.controller('ctrl', function ($scope, $http) {
+    $http.get('https://jsonplaceholder.typicode.com/posts').then(function (response) {
+        $scope.names = response.data
+    });
+    $scope.clicked = function () {
+        window.location = "#/Table.html";
+    }
+});
---
+++ WebApplication6/main.js
@@ -0,0 +1,28 @@
+﻿var myapp = angular.module('myapp', ['ngRoute']);
+myapp.config(['$routeProvider', '$locationProvider',
+    function ($routeprovider, $locationProvider)]);
+
+myapp.controller('loginctrl', function ($scope, $location,$state) {
+    $scope.Login = function () {
+        if ($scope.txtun == 'faisal.khan' &&
+            $scope.txtpw == '12345') {
+
+            alert('loginsuccess');
+            $location.path('/home');
+       
+    }
+        
+        else {
+            alert('loginfailed!');
+            $location.path('/home');
+        }
+    }
+});
+
+
+myapp.controller('ctrl', function ($scope, $http) {
+    $http.get('https://jsonplaceholder.typicode.com/posts').then(function (response) {
+        $scope.names = response.data
+    });
+
+});
